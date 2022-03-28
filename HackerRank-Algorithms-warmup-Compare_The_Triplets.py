c8ce616d12bd567a58d17c4c1305ea1e190d1f63
Hariprasath G <hariprasath167@gmail.com>
2022-03-28 15:01:42 +0530

    HackerRank-Algorithms-warmup-Compare_The_Triplets

--- HackerRank-Algorithms-warmup-Compare_The_Triplets.py
+++ HackerRank-Algorithms-warmup-Compare_The_Triplets.py
@@ -3,12 +3,13 @@ def compareTriplets(a, b):
     Bob = 0
     if len(a) == len(b):
         for i in range(0, len(a)):
-            if a[i] > b[i]:
-                Alice += 1
-            elif a[i] == b[i]:
-                continue
-            elif a[i] < b[i]:
-                Bob += 1
+            if a[i] <= 100 and a[i] >= 1 and b[i] <= 100 and b[i] >= 1: 
+                if a[i] > b[i]:
+                    Alice += 1
+                elif a[i] == b[i]:
+                    continue
+                elif a[i] < b[i]:
+                    Bob += 1
     else:
         print("Length of Array Mismatching.. Error!!!")
 
